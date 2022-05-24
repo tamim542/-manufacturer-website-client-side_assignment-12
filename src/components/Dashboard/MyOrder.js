64301e098cdea43a5efee1ad263ea38ef9f54770
unknown <mdtauhidulislam509@gmail.com>
2022-05-25 01:39:26 +0600

    my order and my profile

--- src/components/Dashboard/MyOrder.js
+++ src/components/Dashboard/MyOrder.js
@@ -5,51 +5,76 @@ import { Button, Card } from 'react-bootstrap';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import auth from '../../firebase.init';
+import './MyOrder.css';
 
 const MyOrder = () => {
 
     const [user] = useAuthState(auth);
     
-    const [items, setItems] = useState([]);
+    const [myorders, setMyorders] = useState([]);
     const navigate = useNavigate();
     useEffect( () => {
         
-        const getItems = async() =>{
-        //    const email = user.email;
-        //     const url = `https://afternoon-badlands-12006.herokuapp.com/item?email=${email}`;
-         
-        //       fetch(url)
-        //       .then(res=>res.json())
-        //       .then(data=>setItems(data))
-            ///---------------
+        const getOrders = async() =>{
+        
             const email = user.email;
            
             const url = `http://localhost:5000/myorder?email=${email}`;
             
             const {data} = await axios.get(url)
-            setItems(data);
+            setMyorders(data);
            
             
         }
-        getItems();
+        getOrders();
 
     }, [user])
-    console.log('items=',items);
+  
+
+ //------------Delete order----------------
+
+ const handleDelete = id =>{
+    const deleteItem = window.confirm('Are you confirm to delete this order?');
+    if(deleteItem){
+        const url = `http://localhost:5000/myorders/${id}`;
+        fetch(url, {
+            method: 'DELETE'
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            const remaining = myorders.filter(item => item._id !== id);
+            setMyorders(remaining);
+        })
+    }
+}
+
 
 
     return (
-        <div>
-           <div class="card w-96 bg-primary text-primary-content">
-  <div class="card-body">
-    <h2 class="card-title">Card title!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div class="card-actions justify-end">
-      <button class="btn">Buy Now</button>
-    </div>
-  </div>
-</div>
+        <div className='mt-16 mb-16 my-order'>
+          
+          {
+              myorders.map(myorder=> <div class="card w-96 bg-base-100 shadow-xl image-full mb-8">
+              <figure><img src={myorder.img} alt="Shoes" /></figure>
+              <div class="card-body">
+                <h2 class="card-title">Product Id: {myorder.productId}</h2>
+                <p>Phone No: {myorder.phoneNo}</p>
+                <p>Address: {myorder.address}</p>
+                <p>Quantity: {myorder.quantity}</p>
+                <div style={{display:'flex'}}>
+                <div class="card-actions justify-end mr-4">
+                  <button class="btn btn-primary">Pay Now</button>
+                </div>
+                <div class="card-actions justify-start">
+                  <button class="btn btn-primary" onClick={() => handleDelete(myorder._id)}>Cancle</button>
+                </div>
+                </div>
+              </div>
+            </div>)
+          }
 
-<p>label:{items.length}</p>
+
         </div>
     );
 };
