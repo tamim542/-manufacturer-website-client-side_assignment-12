5c2cc16564e4fbe0350e4959c71212fb0b92986b
unknown <mdtauhidulislam509@gmail.com>
2022-05-25 19:42:41 +0600

    my portfolio half

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,48 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h1>i i am about page</h1>
+            
+            {/*      tailwind daisyui table      */}
+
+            <div className='ml-28 mr-28' >
+                <h4 className='mt-10' style={{fontWeight:'bold'}}>Loyal Digital platform Limited</h4>
+                One of the largest retail chain stores for Digital in Bangladesh. Laptops Parts are Our main product. Besides, Loyal Digital platform Limited provide product-related services. We have branches in Dhaka Bhaban, Narsingdi Rampura, Gazipur Chumohoni, Dhaka Madaripur, Sibpur Masimpur, Dhaka Gandaria, Chattogram-Agrabad, Chattogram-GEC Circle, Bogura, Rangpur, Mymensingh, Barisal, Rajshahi and Khulna.
+
+                <p className='mt-8 pb-8'>Other Concerns of Loyal Digital platform Limited</p>
+
+                <p>Loyal Digital platform Limited Archives Limited, The Pioneer digital archive house of Bangladesh, grabbing largest amount of data every moment from print and electronic media and the leading service provider for media monitoring as well as.</p>
+
+            </div>
+
+            <div class="overflow-x-auto mb-10 mt-10">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Our Management</th>
+
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th>Chairman: Tanveer Bhuiyan</th>
+
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th>Managing Director: Tanjina Afruj</th>
+
+                        </tr>
+                        <tr>
+                            <th></th>
+                            <th>Director: Tamim Bhuiyan</th>
+
+                        </tr>
+
+                    </thead>
+                </table>
+            </div>
+
         </div>
     );
 };
--- src/components/MyPortfolio/MyPortfolio.js
+++ src/components/MyPortfolio/MyPortfolio.js
@@ -2,8 +2,87 @@ import React from 'react';
 
 const MyPortfolio = () => {
     return (
-        <div>
-            
+        <div className='ml-16 mr-16'>
+            <div class="overflow-x-auto mt-10 mb-10">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <td>Name: Md. Tauhidul Islam Bhuiyan</td>
+                            <td>Email: mdtauhidulislam509@gmail.com</td>
+
+                        </tr>
+                    </thead>
+
+                </table>
+            </div>
+
+
+
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    {/* <!-- head --> */}
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>EDUCATIONAL QUALIFICATIONS </th>
+                            <th>Year</th>
+
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {/* <!-- row 1 --> */}
+                        <tr>
+                            <th>1</th>
+                            <td>B.Sc in Computer Science and
+                                Engineering
+                                East West University, Dhaka
+                                CGPA: 3.12</td>
+                            <td>2016 -2021</td>
+
+                        </tr>
+                        {/* <!-- row 2 --> */}
+                        <tr class="hover">
+                            <th>2</th>
+                            <td>Higher Secondary Certificate
+                                Urea Sar Karkhana College,
+                                Palash, Narsingdi
+                                Group-Science
+                                GPA: 3.75</td>
+                            <td>2015 </td>
+
+                        </tr>
+                        {/* <!-- row 3 --> */}
+                        <tr>
+                            <th>3</th>
+                            <td>Secondary School Certificate
+                                Khanepur High School,
+                                Palash, Narsingdi
+                                Group-Science
+                                GPA: 4.63</td>
+                            <td>2013</td>
+
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div>
+                <table>
+                <tr>
+                            <th>3</th>
+                            <td>Secondary School Certificate
+                                Khanepur High School,
+                                Palash, Narsingdi
+                                Group-Science
+                                GPA: 4.63</td>
+                            <td>2013</td>
+
+                        </tr>
+                </table>
+            </div>
+
         </div>
     );
 };
